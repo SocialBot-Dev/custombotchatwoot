79c5bf99509db4ad01e1cabfc03e9b1451321655
WevrLabs <66124192+WevrLabs@users.noreply.github.com>
2020-07-09 19:33:42 +0200

    Update ChatHeaderExpanded.vue

--- app/javascript/widget/components/ChatHeaderExpanded.vue
+++ app/javascript/widget/components/ChatHeaderExpanded.vue
@@ -2,28 +2,28 @@
   <header class="header-expanded">
     <img v-if="avatarUrl" class="logo" :src="avatarUrl" />
     <span class="close close-button" @click="closeWindow"></span>
-    <span class="header-elements" v-html="introheaderElements">
+    <span class="header-elements">
         <h2 class="title" v-html="introHeading"></h2>
-        <span class="reply-eta" v-html="introReplyETA">Usually replies within 1 hour</span>
-        <span class="social-links" v-html="introsocialLinks">
-            <span class="email" v-html="introemail">
-                <a href="mailto:care@wevrlabs.net" target="_blank" rel="noopener noreferrer" v-html="introemailLink">
-                    <i class="fas fa-envelope" v-html="introemailIcon"></i>
+        <span class="reply-eta">Usually replies within 1 hour</span>
+        <span class="social-links">
+            <span class="email">
+                <a href="mailto:care@wevrlabs.net" target="_blank" rel="noopener noreferrer">
+                    <i class="fas fa-envelope"></i>
                 </a>
             </span>
-            <span class="whatsapp" v-html="introwhatsapp">
-                <a href="https://wa.me/19712514959" target="_blank" rel="noopener noreferrer" v-html="introwhatsappLink">
-                    <i class="fab fa-whatsapp" v-html="introwhatsappIcon"></i>
+            <span class="whatsapp">
+                <a href="https://wa.me/19712514959" target="_blank" rel="noopener noreferrer">
+                    <i class="fab fa-whatsapp"></i>
                 </a>
             </span>
-            <span class="facebook" v-html="introfb">
-                <a href="https://fb.me/WevrLabs" target="_blank" rel="noopener noreferrer" v-html="introfbLink">
-                    <i class="fab fa-facebook-f" v-html="introfbIcon"></i>
+            <span class="facebook">
+                <a href="https://fb.me/WevrLabs" target="_blank" rel="noopener noreferrer">
+                    <i class="fab fa-facebook-f"></i>
                 </a>
             </span>
-            <span class="twitter" v-html="introtwitter">
-                <a href="https://twitter.com/WevrLabs" target="_blank" rel="noopener noreferrer" v-html="introtwitterLink">
-                    <i class="fab fa-twitter" v-html="introtwitterIcon"></i>
+            <span class="twitter">
+                <a href="https://twitter.com/WevrLabs" target="_blank" rel="noopener noreferrer">
+                    <i class="fab fa-twitter"></i>
                 </a>
             </span>
         </span>
@@ -47,68 +47,6 @@ export default {
       type: String,
       default: '',
     },
-    
-    introheaderElements: {
-      type: String,
-      default: '',
-    },
-    introReplyETA: {
-      type: String,
-      default: '',
-    },
-    introsocialLinks: {
-      type: String,
-      default: '',
-    },
-    introemail: {
-      type: String,
-      default: '',
-    },
-    introemailLink: {
-      type: String,
-      default: '',
-    },
-    introemailIcon: {
-      type: String,
-      default: '',
-    },
-    introwhatsapp: {
-      type: String,
-      default: '',
-    },
-    introwhatsappLink: {
-      type: String,
-      default: '',
-    },
-    introwhatsappIcon: {
-      type: String,
-      default: '',
-    },
-    introfb: {
-      type: String,
-      default: '',
-    },
-    introfbLink: {
-      type: String,
-      default: '',
-    },
-    introfbIcon: {
-      type: String,
-      default: '',
-    },
-    introtwitter: {
-      type: String,
-      default: '',
-    },
-    introtwitterLink: {
-      type: String,
-      default: '',
-    },
-    introtwitterIcon: {
-      type: String,
-      default: '',
-    },
-    
     introBody: {
       type: String,
       default: '',
