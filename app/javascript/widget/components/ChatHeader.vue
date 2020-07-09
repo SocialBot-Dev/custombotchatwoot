8dff8918c324e1ff93a2797496366238bfee1405
WevrLabs <66124192+WevrLabs@users.noreply.github.com>
2020-07-09 19:30:29 +0200

    Update ChatHeader.vue

--- app/javascript/widget/components/ChatHeader.vue
+++ app/javascript/widget/components/ChatHeader.vue
@@ -2,7 +2,32 @@
   <header class="header-collapsed">
     <div class="header-branding">
       <img v-if="avatarUrl" :src="avatarUrl" alt="avatar" />
-      <h2 class="title" v-html="title"></h2>
+       <span class="header-elements" v-html="introheaderElements">
+        <h2 class="title" v-html="title"></h2>
+        <span class="reply-eta" v-html="introReplyETA">Usually replies within 1 hour</span>
+        <span class="social-links" v-html="introsocialLinks">
+            <span class="email" v-html="introemail">
+                <a href="mailto:care@wevrlabs.net" target="_blank" rel="noopener noreferrer" v-html="introemailLink">
+                    <i class="fas fa-envelope" v-html="introemailIcon"></i>
+                </a>
+            </span>
+            <span class="whatsapp" v-html="introwhatsapp">
+                <a href="https://wa.me/19712514959" target="_blank" rel="noopener noreferrer" v-html="introwhatsappLink">
+                    <i class="fab fa-whatsapp" v-html="introwhatsappIcon"></i>
+                </a>
+            </span>
+            <span class="facebook" v-html="introfb">
+                <a href="https://fb.me/WevrLabs" target="_blank" rel="noopener noreferrer" v-html="introfbLink">
+                    <i class="fab fa-facebook-f" v-html="introfbIcon"></i>
+                </a>
+            </span>
+            <span class="twitter" v-html="introtwitter">
+                <a href="https://twitter.com/WevrLabs" target="_blank" rel="noopener noreferrer" v-html="introtwitterLink">
+                    <i class="fab fa-twitter" v-html="introtwitterIcon"></i>
+                </a>
+            </span>
+        </span>
+    </span>
     </div>
     <span class="close-button" @click="closeWindow"></span>
   </header>
@@ -23,6 +48,68 @@ export default {
       type: String,
       default: '',
     },
+    
+    introheaderElements: {
+      type: String,
+      default: '',
+    },
+    introReplyETA: {
+      type: String,
+      default: '',
+    },
+    introsocialLinks: {
+      type: String,
+      default: '',
+    },
+    introemail: {
+      type: String,
+      default: '',
+    },
+    introemailLink: {
+      type: String,
+      default: '',
+    },
+    introemailIcon: {
+      type: String,
+      default: '',
+    },
+    introwhatsapp: {
+      type: String,
+      default: '',
+    },
+    introwhatsappLink: {
+      type: String,
+      default: '',
+    },
+    introwhatsappIcon: {
+      type: String,
+      default: '',
+    },
+    introfb: {
+      type: String,
+      default: '',
+    },
+    introfbLink: {
+      type: String,
+      default: '',
+    },
+    introfbIcon: {
+      type: String,
+      default: '',
+    },
+    introtwitter: {
+      type: String,
+      default: '',
+    },
+    introtwitterLink: {
+      type: String,
+      default: '',
+    },
+    introtwitterIcon: {
+      type: String,
+      default: '',
+    },
+    
   },
   computed: {
     ...mapGetters({
