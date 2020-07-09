b4d24861d183a970b0f3bb94d6efd4e3c7ffdc9c
WevrLabs <66124192+WevrLabs@users.noreply.github.com>
2020-07-09 23:28:36 +0200

    Update ChatHeader.vue

--- app/javascript/widget/components/ChatHeader.vue
+++ app/javascript/widget/components/ChatHeader.vue
@@ -69,7 +69,7 @@ export default {
 <style scoped lang="scss">
 @import '~widget/assets/scss/variables.scss';
 @import '~widget/assets/scss/mixins.scss';
-@import "font-awesome";
+@import url('https://dash.wevrlabs.net/assets/css/fontawesome-all.min.css');
 
 .header-collapsed {
   display: flex;
