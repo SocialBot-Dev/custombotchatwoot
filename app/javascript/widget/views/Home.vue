00eb87dc171bfc3ab5a5b66aa1ebbc22f99a77a4
WevrLabs <66124192+WevrLabs@users.noreply.github.com>
2020-07-10 16:14:11 +0200

    Update Home.vue

--- app/javascript/widget/views/Home.vue
+++ app/javascript/widget/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <div
-    class="header-wrap" style="background: rgb(18, 56, 199);">
+    class="header-wrap" style="background: #1aa022;">
       <ChatHeaderExpanded
         v-if="isHeaderExpanded && !hideWelcomeHeader"
         :intro-heading="introHeading"
@@ -118,7 +118,7 @@ export default {
     flex-shrink: 0;
     border-radius: 0;
     background: white;
-    background-image: linear-gradient(125deg,rgba(255, 255, 255, 0.15) -10%,rgba(0,0,0,0.45)) !important;
+    background-image: linear-gradient(125deg,rgba(255, 255, 255, 0.15) -20%,rgba(0,0,0,0.45)) !important;
     z-index: 99;
     @include shadow-large;
 
