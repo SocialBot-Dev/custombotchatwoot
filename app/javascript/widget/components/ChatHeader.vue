9e7e6a5b617a5a051bb9941c793a2bbcb41a4a9a
WevrLabs <66124192+WevrLabs@users.noreply.github.com>
2020-07-17 17:08:24 +0200

    Update ChatHeader.vue

--- app/javascript/widget/components/ChatHeader.vue
+++ app/javascript/widget/components/ChatHeader.vue
@@ -4,7 +4,7 @@
       <img v-if="avatarUrl" :src="avatarUrl" alt="avatar" />
        <span class="header-elements">
         <h2 class="title" v-html="title"></h2>
-        <span v-if="showAvailableAgents" class="reply-eta">
+        <span v-if="showAvailableAgents" :agents="availableAgents" class="reply-eta">
           Online (typically replies within 20min)
         </span>
         <span v-else class="reply-eta">
