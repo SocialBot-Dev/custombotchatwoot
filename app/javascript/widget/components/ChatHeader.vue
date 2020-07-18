18a5bfef2c32fedeca69801dc391f96b56696f4d
WevrLabs <66124192+WevrLabs@users.noreply.github.com>
2020-07-18 23:54:08 +0200

    Update ChatHeader.vue

--- app/javascript/widget/components/ChatHeader.vue
+++ app/javascript/widget/components/ChatHeader.vue
@@ -6,6 +6,12 @@
         <h2 class="title" v-html="title"></h2>
         <span class="reply-eta" v-html="introBody"></span>
         <span class="social-links">
+            <span class="text-link">
+                <a href="https://dash.wevrlabs.net/knowledgebase.php?utm_source=chat_widget" rel="noopener noreferrer">
+                  <i class="fa fa-book"></i>
+                  <span>Help Center</span>
+                </a>
+            </span>
             <span class="email">
                 <a href="mailto:care@wevrlabs.net" target="_blank" rel="noopener noreferrer">
                     <i class="fas fa-envelope"></i>
@@ -133,6 +139,22 @@ export default {
       margin: 5px 0 0;
       display: block;
       
+       .text-link a {
+            width: auto !important;
+            height: 15px !important;
+            text-decoration: none;
+            line-height: 1.4 !important;
+            white-space: nowrap;
+            border-radius: 50px !important;
+            font-weight: 300 !important;
+            color: #eaeaea;
+            font-size: 12px;
+            
+            span {
+                margin: 0 1px 0 5px;
+            }
+        }
+        
        span a {
           background: rgba(0, 0, 0, 0.25);
           display: inline-block;
