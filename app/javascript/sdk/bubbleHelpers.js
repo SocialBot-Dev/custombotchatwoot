a46aab0cd4f10d00c46908f43fd5c95049db5500
WevrLabs <66124192+WevrLabs@users.noreply.github.com>
2020-07-19 11:31:42 +0200

    Update bubbleHelpers.js

--- app/javascript/sdk/bubbleHelpers.js
+++ app/javascript/sdk/bubbleHelpers.js
@@ -2,7 +2,7 @@ import { addClass, toggleClass, wootOn } from './DOMHelpers';
 import { IFrameHelper } from './IFrameHelper';
 
 export const bubbleImg =
-  'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAYAAABXAvmHAAAABmJLR0QA/wD/AP+gvaeTAAABX0lEQVRoge2YO07EMBRFz0MsAJgSWqCDkiVQsA9KFkADCwBWwmcBrIKeLkPJwEzL59IEKWNMwI4mdkY+UqTEkZ178vJxAoVCIQhJR5LG6o+ZpDtJu748FiFQAVudz0Q4L8C+mVXNxpUEQWJZBy7dxhiBY2DcOU4ch25D8CXUN5LU3DazucxDuoS8FIHUeAUk7Ui6rZ/BfTznbyRtxwj8uIklbQIPwChmwA5MgD0ze3LyBN/EF/QfHmADuArt5KvAcz1YCqZmtubkaa2AT+ANWF1IvH/gBizvgdwpAqlZSoHsZ6hNlrICg6II9EDz87Vydw5B4PsbvKrX5/DNhT5IKObOdf5iCBVopQikpgikJjeBaWiH3ATuQzvk9B6YAAdm9hjSKYcKzIBrIsJ7kfTZ8hftrPMBFk2LQP7h4VeBYYQHkPTqhD9PnSkISSeS3uvlNHWeKCSNJKX4wVsoFAL5AjG0pIfpuTtDAAAAAElFTkSuQmCC'
+  '/chat-icon.svg'
 
 export const body = document.getElementsByTagName('body')[0];
 export const widgetHolder = document.createElement('div');
