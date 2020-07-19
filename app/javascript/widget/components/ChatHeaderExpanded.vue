d6fb5ef816e7e3a35498f593b6cfd7c3134a6a88
WevrLabs <66124192+WevrLabs@users.noreply.github.com>
2020-07-19 18:21:45 +0200

    Update ChatHeaderExpanded.vue

--- app/javascript/widget/components/ChatHeaderExpanded.vue
+++ app/javascript/widget/components/ChatHeaderExpanded.vue
@@ -8,7 +8,7 @@
     <span class="header-elements">
         <span class="social-links">
             <span class="text-link">
-                <a href="https://dash.wevrlabs.net/knowledgebase.php?utm_source=chat_widget" rel="noopener noreferrer">
+                <a href="https://dash.wevrlabs.net/knowledgebase.php?utm_source=chat_widget" target="_blank" rel="noopener noreferrer">
                   <i class="fa fa-book"></i>
                   <span>Help Center</span>
                 </a>
@@ -156,20 +156,21 @@ export default {
       margin: 7px 0 5px;
       display: block;
        
-       .text-link a {
-            width: auto !important;
-            height: 15px !important;
-            text-decoration: none;
-            line-height: 1.4 !important;
-            white-space: nowrap;
-            border-radius: 50px !important;
-            font-weight: 300 !important;
-            color: #eaeaea;
-            font-size: 12px;
-            
-            span {
-                margin: 0 1px 0 5px;
-            }
+       .text-link {         
+            a {
+              width: auto !important;
+              height: 15px !important;
+              text-decoration: none;
+              line-height: 1.4 !important;
+              white-space: nowrap;
+              border-radius: 50px !important;
+              font-weight: 300 !important;
+              color: #eaeaea;
+              font-size: 12px;
+                span {
+                    margin: 0 1px 0 5px;
+                }
+              }
         }
 
        span a {
