add878a3e809c43e680be7664c616fee6dd05cec
WevrLabs <wevrlabs@gmail.com>
2020-11-24 15:06:46 +0200

    sync

--- app/javascript/shared/helpers/AudioNotificationHelper.js
+++ app/javascript/shared/helpers/AudioNotificationHelper.js
@@ -1,4 +1,4 @@
-const notificationAudio = require('~/shared/assets/audio/ding.mp3');
+const notificationAudio = require('/shared/assets/audio/ding.mp3');
 
 export const playNotificationAudio = () => {
   try {
