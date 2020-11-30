e5fd1c5db054ce7b8f9d1756ec1c69c3c4dd01c4
WevrLabs <wevrlabs@gmail.com>
2020-11-30 17:35:50 +0200

    Update ChatHeader.vue

--- app/javascript/widget/components/ChatHeader.vue
+++ app/javascript/widget/components/ChatHeader.vue
@@ -20,7 +20,8 @@
             <span class="availability-status" v-if="availableAgents.length">Online</span>
             <span class="availability-status" v-else>Away</span>
          </div>
-        <span class="reply-eta">Availability from 9am to 9pm (GMT +2)</span>
+        <span class="reply-eta" v-if="availableAgents.length">Average response time is 30 minutes</span>
+        <span class="reply-eta" v-else>We'll get back to you in a few hours</span>
         <span class="social-links">
             <span class="text-link">
                 <a>
