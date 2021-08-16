1d730b96223fbfd0e781a9a0abda223dfd6c8ceb
WevrLabs <wevrlabs@gmail.com>
2021-08-16 18:28:25 +0200

    Update sdk.js

--- app/javascript/sdk/sdk.js
+++ app/javascript/sdk/sdk.js
@@ -5,7 +5,7 @@ export const SDK_CSS = `.woot-widget-holder {
   transform: translateY(0);
   overflow: hidden !important;
   position: fixed !important;
-  transition: opacity 0.3s ease-in-out, transform 0.33s ease-in-out;
+  transition: opacity 0.3s ease-in-out, transform 0.5s cubic-bezier(0.1, 0.51, 1, 1);
   z-index: 2147483000 !important;
 }
 
