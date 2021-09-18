592e948b9878c631b1913d5100b112d9abaa9fd0
WevrLabs <wevrlabs@gmail.com>
2021-09-18 23:13:16 +0200

    Update incoming_message_service.rb

--- app/services/telegram/incoming_message_service.rb
+++ app/services/telegram/incoming_message_service.rb
@@ -95,7 +95,7 @@ class Telegram::IncomingMessageService
 
   def attach_files
     file = params[:message][:document]
-    file ||= params[:message][:sticker][:thumb]
+    file ||= params[:message][:sticker][:thumb][:file_id]
     file ||= params[:message][:photo]&.last
 
     return unless file
