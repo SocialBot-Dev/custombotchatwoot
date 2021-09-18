2ab2079b89aa53c272b184f3347a640f5245661a
WevrLabs <wevrlabs@gmail.com>
2021-09-18 20:28:31 +0200

    Update file_type_helper.rb

--- app/helpers/file_type_helper.rb
+++ app/helpers/file_type_helper.rb
@@ -1,13 +1,6 @@
 module FileTypeHelper
   def file_type(content_type)
-    return :image if [
-      'image/jpeg',
-      'image/png',
-      'image/gif',
-      'image/tiff',
-      'image/bmp',
-      'image/webp'
-    ].include?(content_type)
+    return :image if content_type.include?('image/')
 
     return :video if content_type.include?('video/')
     return :audio if content_type.include?('audio/')
