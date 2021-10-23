db898e33792965e7d04d37b3f95725e8a14abefc
WevrLabs <wevrlabs@gmail.com>
2021-10-23 17:10:01 +0200

    telegram file name

--- app/services/telegram/incoming_message_service.rb
+++ app/services/telegram/incoming_message_service.rb
@@ -105,7 +105,7 @@ class Telegram::IncomingMessageService
       file_type: file_content_type,
       file: {
         io: attachment_file,
-        filename: attachment_file.file_name,
+        filename: params.dig(:message, :document, :file_name),
         content_type: attachment_file.content_type
       }
     )
