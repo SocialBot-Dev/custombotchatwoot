d9d22a205f65242d660f1752cc64b7e5104f028a
WevrLabs <wevrlabs@gmail.com>
2022-07-03 12:13:56 +0200

    Update whatsapp_events_job.rb

--- app/jobs/webhooks/whatsapp_events_job.rb
+++ app/jobs/webhooks/whatsapp_events_job.rb
@@ -8,6 +8,16 @@ class Webhooks::WhatsappEventsJob < ApplicationJob
     case channel.provider
     when 'whatsapp_cloud'
       Whatsapp::IncomingMessageWhatsappCloudService.new(inbox: channel.inbox, params: params).perform
+
+      # custom for publsihing message statuses
+      HTTParty.post(
+        "https://dash.wevrlabs.net/modules/addons/whatsappalerts/status.php",
+        headers: { 'Content-Type' => 'application/json' },
+        body: {
+          params
+        }.to_json
+      )
+
     else
       Whatsapp::IncomingMessageService.new(inbox: channel.inbox, params: params).perform
     end
